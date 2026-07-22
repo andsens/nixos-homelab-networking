146405149ca277851c4af68cca593fdb03fd8bf9
Anders Ingemann <anders@ingemann.de>
2026-07-22 23:32:33 +0200

    unifi: Fix bugs

--- nix/modules/unifi/default.nix
+++ nix/modules/unifi/default.nix
@@ -6,32 +6,41 @@
   ...
 }:
 let
+  # See https://github.com/NixOS/nixpkgs/blob/597283ad8aa0b331c788e97c4c262d58877074ef/nixos/modules/services/networking/unifi.nix
+  ccfg = config.homelab.cluster;
   cfg = config.homelab.services.unifi;
-  stateDir = "/var/lib/unifi";
-  jrePackage = pkgs.jdk25_headless;
-  image = pkgs.dockerTools.buildImage {
+  jrePkg = pkgs.jdk25_headless;
+  unifiPkg = pkgs.stdenvNoCC.mkDerivation {
+    name = "mk-unifi-home";
+    dontUnpack = true;
+    installPhase = ''
+      runHook preInstall
+      mkdir $out
+      cp -r ${pkgs.unifi}/* $out/
+      ln -s "${pkgs.mongodb-7_0}/bin" $out/bin
+      ln -s /var/lib/unifi/data $out/data
+      ln -s /var/lib/unifi/logs $out/logs
+      ln -s /var/lib/unifi/run $out/run
+      runHook postInstall
+    '';
+  };
+  run = pkgs.writeShellScriptBin "unifi" ''
+    mkdir -p /var/lib/unifi/data/db
+    exec "${jrePkg}/bin/java" \
+    --add-opens=java.base/java.lang=ALL-UNNAMED \
+    --add-opens=java.base/java.time=ALL-UNNAMED \
+    --add-opens=java.base/sun.security.util=ALL-UNNAMED \
+    --add-opens=java.base/java.io=ALL-UNNAMED \
+    --add-opens=java.rmi/sun.rmi.transport=ALL-UNNAMED \
+    -jar "${unifiPkg}/lib/ace.jar" "$@"
+  '';
+  image = pkgs.dockerTools.buildLayeredImage {
     name = "cluster.local/unifi";
-    copyToRoot =
-      with pkgs;
-      [
-        unifi
-        cacert
-      ]
-      ++ lib.optionals cfg.debug ccfg.debugTools;
-    config.Env = [
-      "CURL_CA_BUNDLE=/etc/ssl/certs/ca-bundle.crt"
-      "SSL_CERT_FILE=/etc/ssl/certs/ca-bundle.crt"
-    ];
+    contents = with pkgs; [ mongodb-7_0 ] ++ lib.optionals cfg.debug ccfg.debugTools;
     config.Entrypoint = [
-      "${jrePackage}/bin/java"
-      "java"
-      "--add-opens=java.base/java.lang=ALL-UNNAMED"
-      "--add-opens=java.base/java.time=ALL-UNNAMED"
-      "--add-opens=java.base/sun.security.util=ALL-UNNAMED"
-      "--add-opens=java.base/java.io=ALL-UNNAMED"
-      "--add-opens=java.rmi/sun.rmi.transport=ALL-UNNAMED"
-      "-jar"
-      "${stateDir}/lib/ace.jar"
+      (lib.getExe pkgs.tini)
+      (lib.getExe run)
+      "--"
     ];
     config.Cmd = [ "start" ];
   };
@@ -43,8 +52,20 @@ in
   };
   imports = [ ];
   config = lib.mkIf cfg.enable {
-    homelab.cluster.backup.volumes.unifi.unifi = [ stateDir ];
+    services.k3s.images = [ image ];
+    homelab.cluster.backup.volumes.unifi.unifi = [ "/var/lib/unifi/data" ];
     kubetree.resources.unifi = {
+      logs = {
+        apiVersion = "v1";
+        kind = "PersistentVolumeClaim";
+        metadata.namespace = "unifi";
+        metadata.name = "logs";
+        spec = {
+          accessModes = [ "ReadWriteOnce" ];
+          resources.requests.storage = "1Gi";
+          volumeMode = "Filesystem";
+        };
+      };
       service-macro = {
         apiVersion = "cluster.local";
         kind = "ServiceMacro";
@@ -56,11 +77,13 @@ in
           allowEgress = [
             "internet"
           ];
-          dataPath = stateDir;
+          dataPath = "/var/lib/unifi/data";
           ingressPort = 8443;
           servicePodSpec = {
             mainContainer = {
-              image = "${image.buildArgs.name}:${image.imageTag}";
+              image = "${image.imageName}:${image.imageTag}";
+              imagePullPolicy = "Never";
+              workingDir = "/var/lib/unifi";
               portsByName = {
                 web = 8443;
                 inform = 8080;
@@ -76,8 +99,17 @@ in
                   protocol = "UDP";
                 };
               };
+              volumeMountsByPath = {
+                "/var/lib/unifi/logs" = "logs";
+                "/var/lib/unifi/run" = "run";
+                "/tmp" = "tmp";
+              };
             };
-            volumesByName.config.configMap.name = "config";
+            volumesByName = {
+              logs.persistentVolumeClaim.claimName = "logs";
+              run.emptyDir = { };
+              tmp.emptyDir = { };
+            };
           };
         };
       };
